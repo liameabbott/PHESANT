6e30eb654ca30cf1f37f7bbf7ad4bc98687bc256
Liam Abbott <liam.e.abbott@gmail.com>
2018-04-05 13:21:31 -0400

    updated phensummary code

--- run_summarise_phenotypes.r
+++ run_summarise_phenotypes.r
@@ -1,205 +1,57 @@
+
 source("summarise_phenotypes.r")
 
-## VERNERI'S APPLICATION.
-
-# Running Verneri's application through.
-hist_filename <- "~/results/ukb1859_hist"
-pheno_summary <- "~/results/ukb1859_phenosummary.tsv"
-
-filename <- "~/results/ukb7127_output"
-tsv_filename <- paste(filename, ".tsv", sep="")
-log_file <- paste(filename, ".log", sep="")
-tsv_data <- read.table(tsv_filename, header=TRUE, sep='\t')
-
-qc_data <- read.table("~/results/ukb1859_qc.tsv", header=TRUE)
-
 outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
-					   sep='\t', quote="", comment.char="", header=TRUE)
+                       sep='\t', quote="", comment.char="", header=TRUE)
 
-samples_for_removal <- as.character(read.table("~/results/w1859_20170726_participantwithdrawallist.csv")$V1)
+for (i in seq(1, 4)) {
 
-notes_for_manny_1859 <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal)
+    tsv_data_both <- read.table(paste0('../ukb11214_rw_restricted_to_QC.', i, '.log', header=TRUE, sep='\t')
+    tsv_data_females <- read.table(paste0('../ukb11214_rw_restricted_to_QC_females.', i, '.log', header=TRUE, sep='\t')
+    tsv_data_males <- read.table(paste0('../ukb11214_rw_restricted_to_QC_males.', i, '.log', header=TRUE, sep='\t')
 
-write.table(notes_for_manny_1859, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
+    log_file_both <- read.table(paste0('../ukb11214_rw_restricted_to_QC.', i, '.log', header=TRUE, sep='\t')
+    log_file_females <- read.table(paste0('../ukb11214_rw_restricted_to_QC_females.', i, '.log', header=TRUE, sep='\t')
+    log_file_males <- read.table(paste0('../ukb11214_rw_restricted_to_QC_males.', i, '.log', header=TRUE, sep='\t')
 
-pheno_summary_PHESANT <- "~/results/ukb1859_phenosummary_final.tsv"
-notes_for_manny_1859_PHESANT_codings_included <- include_PHESANT_reassignment_names(pheno_summary, outcome_info)
-write.table(notes_for_manny_1859_PHESANT_codings_included, file=pheno_summary_PHESANT, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
+    notes_both <- get_notes(tsv_data_both, log_file_both, outcome_info, codings_tables)
+    write.table(notes_both, file=paste0('../ukb31063.both_sexes.tmp.', i, '.tsv'), col.names=TRUE, row.names=FALSE, sep='\t', quote=FALSE)
 
+    notes_females <- get_notes(tsv_data_females, log_file_females, outcome_info, codings_tables)
+    write.table(notes_females, file=paste0('../ukb31063.females.tmp.', i, '.tsv'), col.names=TRUE, row.names=FALSE, sep='\t', quote=FALSE)
 
-# Check against the samples you get if using Liam's collection.
-hist_filename <- "~/results/ukb1859_hist_check"
-pheno_summary <- "~/results/ukb1859_phenosummary_check.tsv"
+    notes_males <- get_notes(tsv_data_males, log_file_males, outcome_info, codings_tables)
+    write.table(notes_males, file=paste0('../ukb31063.males.tmp.', i, '.tsv'), col.names=TRUE, row.names=FALSE, sep='\t', quote=FALSE)
 
-filename <- "~/results/ukb7127_output"
-tsv_filename <- paste(filename, ".tsv", sep="")
-log_file <- paste(filename, ".log", sep="")
-tsv_data <- read.table(tsv_filename, header=TRUE, sep='\t')
+    notes_codings_included_both <- include_PHESANT_reassignment_names(paste0('../ukb31063.both_sexes.tmp.', i, '.tsv'), outcome_info)
+    write.table(notes_codings_included_both, file=paste0('../ukb31063.both_sexes.phenosummary.', i, '.tsv'), col.names=TRUE, row.names=FALSE, sep='\t', quote=FALSE)
 
-qc_data <- ""
+    notes_codings_included_females <- include_PHESANT_reassignment_names(paste0('../ukb31063.females.tmp.', i, '.tsv'), outcome_info)
+    write.table(notes_codings_included_both, file=paste0('../ukb31063.females.phenosummary.', i, '.tsv'), col.names=TRUE, row.names=FALSE, sep='\t', quote=FALSE)
 
-outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
-					   sep='\t', quote="", comment.char="", header=TRUE)
+    notes_codings_included_males <- include_PHESANT_reassignment_names(paste0('../ukb31063.males.tmp.', i, '.tsv'), outcome_info)
+    write.table(notes_codings_included_both, file=paste0('../ukb31063.males.phenosummary.', i, '.tsv'), col.names=TRUE, row.names=FALSE, sep='\t', quote=FALSE)
 
-samples_for_inclusion <- as.character(read.table("~/results/ukb1859_qc.sample_list", header=TRUE)$sample)
-check <- TRUE
-
-notes_for_manny_1859 <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal, samples_for_inclusion, check)
-
-write.table(notes_for_manny_1859, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-pheno_summary_PHESANT <- "~/results/ukb1859_phenosummary_check_final.tsv"
-notes_for_manny_1859_PHESANT_codings_included <- include_PHESANT_reassignment_names(pheno_summary, outcome_info)
-write.table(notes_for_manny_1859_PHESANT_codings_included, file=pheno_summary_PHESANT, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-# Summary of ALL the samples in Verneri's application.
-hist_filename <- "~/results/ukb1859_hist_all"
-pheno_summary <- "~/results/ukb1859_phenosummary_all.tsv"
-
-filename <- "~/results/ukb7127_output"
-tsv_filename <- paste(filename, ".tsv", sep="")
-log_file <- paste(filename, ".log", sep="")
-tsv_data <- read.table(tsv_filename, header=TRUE, sep='\t')
-
-qc_data <- ""
-
-outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
-					   sep='\t', quote="", comment.char="", header=TRUE)
-
-samples_for_removal <- ""
-samples_for_inclusion <- tsv_data$userId
-check <- FALSE
-
-notes_for_manny_1859 <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal, samples_for_inclusion, check)
-
-write.table(notes_for_manny_1859, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-pheno_summary_PHESANT <- "~/results/ukb1859_phenosummary_all_final.tsv"
-notes_for_manny_1859_PHESANT_codings_included <- include_PHESANT_reassignment_names(pheno_summary, outcome_info)
-write.table(notes_for_manny_1859_PHESANT_codings_included, file=pheno_summary_PHESANT, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
+}
 
 
-## JOEL'S APPLICATION.
+#log_both <- read.table('../')
+#filename <- "../ukb31063.output"
+#tsv_filename <- paste(filename, ".tsv", sep="")
+#log_file <- paste(filename, ".log", sep="")
 
-# Running Joel's application through.
-hist_filename <- "~/results/ukb1189_hist"
-pheno_summary <- "~/results/ukb1189_phenosummary.tsv"
 
-filename <- "~/results/ukb1189_output"
-tsv_filename <- paste(filename, ".tsv", sep="")
-log_file <- paste(filename, ".log", sep="")
-tsv_data <- read.table(tsv_filename, header=TRUE, sep='\t')
+#qc_data <- read.table("~/results/ukb1859_qc.tsv", header=TRUE)
 
-qc_data <- read.table("~/results/ukb1189_qc.tsv", header=TRUE)
+#outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
+#					   sep='\t', quote="", comment.char="", header=TRUE)
 
-outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
-					   sep='\t', quote="", comment.char="", header=TRUE)
+#samples_for_removal <- as.character(read.table("~/results/w1859_20170726_participantwithdrawallist.csv")$V1)
 
-samples_for_removal <- as.character(read.table("~/results/w1189_20170726_participantwithdrawallist.csv")$V1)
+#notes_for_manny_1859 <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal)
 
-notes_for_manny_1189 <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal)
+#write.table(notes_for_manny_1859, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
 
-write.table(notes_for_manny_1189, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-pheno_summary_PHESANT <- "~/results/ukb1189_phenosummary_final.tsv"
-notes_for_manny_1189_PHESANT_codings_included <- include_PHESANT_reassignment_names(pheno_summary, outcome_info)
-write.table(notes_for_manny_1189_PHESANT_codings_included, file=pheno_summary_PHESANT, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-# Check against the samples you get if using Liam's collection.
-hist_filename <- "~/results/ukb1189_hist_check"
-pheno_summary <- "~/results/ukb1189_phenosummary_check.tsv"
-
-filename <- "~/results/ukb1189_output"
-tsv_filename <- paste(filename, ".tsv", sep="")
-log_file <- paste(filename, ".log", sep="")
-tsv_data <- read.table(tsv_filename, header=TRUE, sep='\t')
-
-qc_data <- ""
-
-outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
-					   sep='\t', quote="", comment.char="", header=TRUE)
-
-samples_for_removal <- ""
-samples_for_inclusion <- as.character(read.table("~/results/ukb1189_qc.sample_list", header=TRUE)$sample)
-
-notes_for_manny_1189 <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal, samples_for_inclusion)
-
-write.table(notes_for_manny_1189, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-pheno_summary_PHESANT <- "~/results/ukb1189_phenosummary_check_final.tsv"
-notes_for_manny_1189_PHESANT_codings_included <- include_PHESANT_reassignment_names(pheno_summary, outcome_info)
-write.table(notes_for_manny_1189_PHESANT_codings_included, file=pheno_summary_PHESANT, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-# Summary of ALL the samples in Joel's application.
-hist_filename <- "~/results/ukb1189_hist_all"
-pheno_summary <- "~/results/ukb1189_phenosummary_all.tsv"
-
-filename <- "~/results/ukb1189_output"
-tsv_filename <- paste(filename, ".tsv", sep="")
-log_file <- paste(filename, ".log", sep="")
-tsv_data <- read.table(tsv_filename, header=TRUE, sep='\t')
-
-qc_data <- ""
-
-outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
-					   sep='\t', quote="", comment.char="", header=TRUE)
-
-samples_for_removal <- ""
-samples_for_inclusion <- as.character(read.table("~/results/ukb1189_qc.sample_list", header=TRUE)$sample)
-
-notes_for_manny_1189 <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal, samples_for_inclusion)
-
-write.table(notes_for_manny_1189, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-pheno_summary_PHESANT <- "~/results/ukb1189_phenosummary_all_final.tsv"
-notes_for_manny_1189_PHESANT_codings_included <- include_PHESANT_reassignment_names(pheno_summary, outcome_info)
-write.table(notes_for_manny_1189_PHESANT_codings_included, file=pheno_summary_PHESANT, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-# Run the ICD10 codes in Joel's application through.
-hist_filename <- "~/results/ukb1189_icd10_hist"
-pheno_summary <- "~/results/ukb1189_icd10_phenosummary.tsv"
-
-filename <- "~/results/ukb1189_icd10_flags.tsv"
-log_file <- FALSE
-tsv_data <- read.table(filename, header=TRUE, sep='\t')
-names(tsv_data)[1] <- "userId"
-names(tsv_data)[-1] <- paste("X41202", names(tsv_data)[-1], sep="_")
-
-qc_data <- read.table("~/results/ukb1189_qc.tsv", header=TRUE)
-
-outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
-					   sep='\t', quote="", comment.char="", header=TRUE)
-
-samples_for_removal <- as.character(read.table("~/results/w1189_20170726_participantwithdrawallist.csv")$V1)
-
-notes_for_manny_1189_icd10 <- get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal, start_column=2)
-write.table(notes_for_manny_1189_icd10, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-## ANDREA'S APPLICATION
-
-# Running Andrea's application through.
-# Can only do 'all', as we don't have the QC information and set of redacted/individuals that have removed consent.
-hist_filename <- "~/results/ukb9438_hist"
-pheno_summary <- "~/results/ukb9438_phenosummary.tsv"
-
-filename <- "~/results/ukb9438_output"
-tsv_filename <- paste(filename, ".tsv", sep="")
-log_file <- paste(filename, ".log", sep="")
-tsv_data <- read.table(tsv_filename, header=TRUE, sep='\t')
-
-qc_data <- ""
-
-outcome_info <- read.table("~/Repositories/PHESANT/variable-info/outcome_info_final.tsv",
-					   sep='\t', quote="", comment.char="", header=TRUE)
-
-samples_for_removal <- ""
-samples_for_inclusion <- tsv_data$userId
-check <- FALSE
-
-notes_for_manny_9438 <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data, samples_for_removal, samples_for_inclusion, check)
-write.table(notes_for_manny_9438, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
-pheno_summary_PHESANT <- "~/results/ukb9438_phenosummary_final.tsv"
-notes_for_manny_9438_PHESANT_codings_included <- include_PHESANT_reassignment_names(pheno_summary, outcome_info)
-write.table(notes_for_manny_9438_PHESANT_codings_included, file=pheno_summary_PHESANT, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
-
+#pheno_summary_PHESANT <- "~/results/ukb1859_phenosummary_final.tsv"
+#notes_for_manny_1859_PHESANT_codings_included <- include_PHESANT_reassignment_names(pheno_summary, outcome_info)
+#write.table(notes_for_manny_1859_PHESANT_codings_included, file=pheno_summary_PHESANT, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
--- summarise_phenotypes.r
+++ summarise_phenotypes.r
@@ -18,65 +18,17 @@ for(i in to_read) {
 
 get_subtype <- function(x) paste(x[2:length(x)],collapse=" ")
 
-get_hists_and_notes <- function(hist_filename, tsv_data, log_file, outcome_info, codings_tables, qc_data,
-	samples_for_removal, samples_for_inclusion=FALSE, check=TRUE, start_column=4)
+get_notes <- function(tsv_data, log_file, outcome_info, codings_tables, start_column=4)
 {	
-	if(samples_for_inclusion == FALSE) {
-		# First, let's restrict to the samples that we want to parse.
-		# - in.white.British.ancestry.subset==1
-		# - used.in.pca.calculation==1
-		# - excess.relatives==0
-		# - putative.sex.chromosome.aneuploidy==0
-		# ...this should leave you with 337208 samples
-
-		where_good_samples <- which(qc_data$in.white.British.ancestry.subset==1 &
-							 	     qc_data$used.in.pca.calculation == 1 &
-							 	     qc_data$excess.relatives == 0 & 
-							 	     qc_data$putative.sex.chromosome.aneuploidy == 0)
-		# Check
-		if(length(where_good_samples) != 337208) {
-			return("failed")
-		}
-
-		# Check that after removal of redacted samples, we have 337205.
-		where_redacted_samples <- which(qc_data$eid %in% c("-1", "-2", "-3"))
-		if(length(where_redacted_samples) != 3) {
-			return("failed - couldn't find all 3 redacted samples")
-		}
-
-		# Check after final removal of the last 6 samples, we have 337199.
-		where_samples_for_removal <- which(qc_data$eid %in% samples_for_removal)
-
-		where_good_samples <- setdiff(where_good_samples, where_redacted_samples)
-		where_good_samples <- setdiff(where_good_samples, where_samples_for_removal)
-
-		if(length(where_good_samples) != 337199) {
-			return("failed - not the correct number of samples after removal of redacted samples and individuals who removed consent")
-		}
-
-		good_samples <- qc_data$eid[where_good_samples]
-
-	} else {
-		good_samples <- samples_for_inclusion
-	}
-
-	tsv_data <- tsv_data[tsv_data$userId %in% good_samples,]
-
-	if(check == TRUE) {
-		if(dim(tsv_data)[1] != 337199) {
-			return("failed - not the correct number of samples after removal of redacted samples and individuals who removed consent")
-		}
-	}
 
 	if (ncol(tsv_data) > (start_column-1)) {
-		pdf(file=paste(hist_filename,".pdf",sep=""), width=5, height=5)
-		par(oma=c(4,0,0,0))
 		# Create character matrix 'notes' that we will write to disk and pass to Manny.
-		notes <- matrix(nrow=(ncol(tsv_data)-start_column+1),ncol=9)
-		colnames(notes) <- c("Field", "N.non.missing", "N.missing", "N.cases", "N.controls",
+		notes <- matrix(nrow=(ncol(tsv_data)-start_column+1),ncol=10)
+		colnames(notes) <- c("FieldID", "Field", "N.non.missing", "N.missing", "N.cases", "N.controls",
 			"Notes", "PHESANT.notes", "PHESANT.reassignments", "warning.for.case.control")
 		# Rownames are the FieldIDs
-		rownames(notes) <- colnames(tsv_data)[start_column:ncol(tsv_data)]
+		#rownames(notes) <- colnames(tsv_data)[start_column:ncol(tsv_data)]
+		notes[,1] = colnames(tsv_data)[start_column:ncol(tsv_data)]
 		samples <- nrow(tsv_data)
 		k <- 1
 		for(i in colnames(tsv_data)[start_column:ncol(tsv_data)]){
@@ -85,24 +37,23 @@ get_hists_and_notes <- function(hist_filename, tsv_data, log_file, outcome_info,
 				var <- substr(i,2,nchar(i))
 				where <- which(outcome_info$FieldID == var)
 				i_name <- paste(trim(outcome_info$Field[where]),"-",i)
-				# The first column is the Field name.
-				notes[k,1] <- trim(outcome_info$Field[where])
-				# The sixth column is the 'Notes' field in variable-info file:
-				notes[k,6] <- remove_excess_whitespace(as.character(trim(outcome_info$Notes[where])))
+				# The second column is the Field name.
+				notes[k,2] <- trim(outcome_info$Field[where])
+				# The seventh column is the 'Notes' field in variable-info file:
+				notes[k,7] <- remove_excess_whitespace(as.character(trim(outcome_info$Notes[where])))
 				
 				if (log_file != FALSE){
-					# The seventh column is information about how the data is parsed using PHESANT:
+					# The eighth column is information about how the data is parsed using PHESANT:
 					matching_line <- trim(grep(paste('^', var, '_', sep=""), readLines(log_file), value=TRUE))
-					notes[k,7] <- matching_line
+					notes[k,8] <- matching_line
 
-					# The eighth column is usually empty, unless there are PHESANT reassignments, in which
+					# The ninth column is usually empty, unless there are PHESANT reassignments, in which
 					# case we detail those reassignments here:
 					if (length(grep("reassignments", matching_line)) > 0) {
-						notes[k,8] <- trim(gsub("^.*(reassignments: .*?)\\|\\|.*", "\\1", matching_line))
+						notes[k,9] <- trim(gsub("^.*(reassignments: .*?)\\|\\|.*", "\\1", matching_line))
 					}
 				}
 
-				hist(tsv_data[i][,1], main=i_name, col="grey", breaks=100, xlab="value")
 			} else if (type=="logical" | type=="integer") {
 				var <- strsplit(i, split="_")[[1]][1]
 				var <- substr(var, 2, nchar(var))
@@ -110,11 +61,11 @@ get_hists_and_notes <- function(hist_filename, tsv_data, log_file, outcome_info,
 				where <- which(outcome_info$FieldID == var)
 				
 				# Add the notes:
-				# The sixth column is the 'notes' field in variable-info file:
-				notes[k,6] <- remove_excess_whitespace(as.character(trim(outcome_info$Notes[where])))
+				# The seventh column is the 'notes' field in variable-info file:
+				notes[k,7] <- remove_excess_whitespace(as.character(trim(outcome_info$Notes[where])))
 				
 				if (log_file != FALSE) {
-					# The seventh column is information about how the data is parsed using PHESANT:
+					# The eight column is information about how the data is parsed using PHESANT:
 					matching_line <- trim(grep(paste('^', var, '_', sep=""), readLines(log_file), value=TRUE))
 					
 					if (length(grep("^.*CAT-MULTIPLE", matching_line)) > 0) {
@@ -122,19 +73,19 @@ get_hists_and_notes <- function(hist_filename, tsv_data, log_file, outcome_info,
 						if(matching_line == new_matching_line){
 							new_matching_line <- paste(gsub(paste("^(.*?)\\|.*(CAT-MUL-BINARY-VAR", subvar, ".*)"), "\\1 \\|\\| \\2", matching_line))
 						}
-						notes[k,7] <- trim(new_matching_line)
+						notes[k,8] <- trim(new_matching_line)
 					} else if (length(grep("CAT-SINGLE-UNORDERED", matching_line)) > 0) {
 						new_matching_line <- gsub(paste("^(.*?)\\|.*(Inc.*?: ", subvar, "\\([0-9]+\\)).*", sep=""),
 							paste("\\1 \\|\\| CAT-SINGLE \\|\\| CAT-SINGLE-BINARY-VAR:", subvar, " \\|\\| \\2 \\|\\|"), matching_line)
-						notes[k,7] <- trim(new_matching_line)
+						notes[k,8] <- trim(new_matching_line)
 					} else {
-						notes[k,7] <- trim(matching_line)
+						notes[k,8] <- trim(matching_line)
 					}
 
-					# The eighth column is usually empty, unless there are PHESANT reassignments, in which
+					# The ninth column is usually empty, unless there are PHESANT reassignments, in which
 					# case we detail those reassignments here:
 					if (length(grep("reassignments", matching_line)) > 0) {
-						notes[k,8] <- trim(gsub("^.*(reassignments: .*?)\\|\\|.*", "\\1", matching_line))
+						notes[k,9] <- trim(gsub("^.*(reassignments: .*?)\\|\\|.*", "\\1", matching_line))
 					}
 				}
 
@@ -151,15 +102,15 @@ get_hists_and_notes <- function(hist_filename, tsv_data, log_file, outcome_info,
 						# The first column is the Field name - if we've entered this if statement,
 						# we need to include the subfield (as the categorical has been split into 
 						# loads of booleans).
-						notes[k,1] <- paste(trim(outcome_info$Field[where]),": ",i_subname, sep="")
+						notes[k,2] <- paste(trim(outcome_info$Field[where]),": ",i_subname, sep="")
 					} else {
-						notes[k,1] <- trim(outcome_info$Field[where])
+						notes[k,2] <- trim(outcome_info$Field[where])
 					}
 				} else {
 					# The first column is the Field name.
-					notes[k,1] <- trim(outcome_info$Field[where])
+					notes[k,2] <- trim(outcome_info$Field[where])
 				}
-				colour <- "grey"
+
 				if(length(grep("Too many", i_subname))>0) {
 					# Then we need to look in the corresponding tsv_data-coding table to get the label.
 					coding <- trim(gsub("^.*id=","",i_subname))
@@ -169,32 +120,30 @@ get_hists_and_notes <- function(hist_filename, tsv_data, log_file, outcome_info,
 					# we need to include the subfield (as the categorical has been split into 
 					# loads of booleans) - this is accessed from the associated coding table for the 
 					# phenotype.
-					notes[k,1] <- paste(trim(outcome_info$Field[where]),": ",i_subname, sep="")
+					notes[k,2] <- paste(trim(outcome_info$Field[where]),": ",i_subname, sep="")
 				}
 				y <- table(tsv_data[i][,1])
-				main <- ifelse(type=="logical", paste(i_name,"\n",i_subname), i_name)
-				xx <- barplot(height = y, main=main, ylim=c(0, 1.1*max(y)))
-				text(x = xx, y = y, label = y, pos = 3, cex = 0.8, col = "red")
+				#main <- ifelse(type=="logical", paste(i_name,"\n",i_subname), i_name)
 
 				# Finally, include the case and control numbers for the logical and integer phenotypes
 				# with just two categories.
 				if(type=="logical") {
-					notes[k,4] <- y[names(y) == "TRUE"]
-					notes[k,5] <- y[names(y) == "FALSE"]
+					notes[k,5] <- y[names(y) == "TRUE"]
+					notes[k,6] <- y[names(y) == "FALSE"]
 				} else {
 					if(length(y) == 2) {
 						# I assume that 1 encodes a positive here:
 						if(all(names(y) == c("0", "1"))) {
-							notes[k,4] <- y[names(y) == "1"]
-							notes[k,5] <- y[names(y) == "0"]
+							notes[k,5] <- y[names(y) == "1"]
+							notes[k,6] <- y[names(y) == "0"]
 						} else {
 							# All bets are off if the variable is not 0,1, 
 							# but I report the numbers in each of the two categories.
 							# This should be the larger number encoding the positive, according to the 
 							# laws of the table function.
-							notes[k,4] <- y[1]
-							notes[k,5] <- y[2]
-							notes[k,9] <- "YES"
+							notes[k,5] <- y[1]
+							notes[k,6] <- y[2]
+							notes[k,10] <- "YES"
 						}
 					}
 				}
@@ -204,19 +153,14 @@ get_hists_and_notes <- function(hist_filename, tsv_data, log_file, outcome_info,
 			}
 			# The third column is the number of missing data for this phenotype.
 			n_miss <- sum(is.na(tsv_data[i][,1]))
-			notes[i,3] <- n_miss
+			notes[i,4] <- n_miss
 			# The second column is the number of non-missing data for this phenotype.
 			n_non_miss <- sum(!is.na(tsv_data[i][,1]))
-			notes[i,2] <- n_non_miss
-			p <- n_miss/samples
-			colour <- ifelse(p > 0.95, "red", "darkgreen")
-			mtext(side=1, text=paste("Missing = ", n_miss, ", Proportion = ", round(p, 2), sep=""), line=0, outer=TRUE, col=colour)
-			if(type=="integer") mtext(side=1, text=i_subname, line=-1, outer=TRUE)
+			notes[i,3] <- n_non_miss
 			k <- k+1
 		}
-		dev.off()
 		# Get rid of the X that's appended at the start of each variable.
-		rownames(notes) <- substr(rownames(notes), 2, nchar(rownames(notes)))
+		notes[,1] <- substr(notes[,1], 2, nchar(notes[,1]))
 		return(notes)
 	}
 }
@@ -226,8 +170,8 @@ include_PHESANT_reassignment_names <- function(pheno_summary, outcome_info)
 	pheno_summary <- read.table(pheno_summary, sep='\t', quote="", header=TRUE, stringsAsFactors=FALSE) 
 
 	# Find the variables that have had a reassignment.
-	where_reassignment <- which(!is.na(pheno_summary[,8]))
-	reassignments <- as.character(pheno_summary[where_reassignment,8])
+	where_reassignment <- which(!is.na(pheno_summary[,9]))
+	reassignments <- as.character(pheno_summary[where_reassignment,9])
 	
 	reassignments_before_after_list <- list()
 	reassignments <- gsub("reassignments: ", "", reassignments)
@@ -237,14 +181,14 @@ include_PHESANT_reassignment_names <- function(pheno_summary, outcome_info)
 	}
 
 	for(i in 1:length(where_reassignment)) {
-		if(length(grep("_", rownames(pheno_summary)[where_reassignment[i]])) > 0) {
+		if(length(grep("_", pheno_summary[,1][where_reassignment[i]])) > 0) {
 			for(j in 1:nrow(reassignments_before_after_list[[i]])) {
 				if(length(grep(paste("_", reassignments_before_after_list[[i]][j,2], sep=""),
-					rownames(pheno_summary)[where_reassignment[i]])) > 0) {
+					pheno_summary[,1][where_reassignment[i]])) > 0) {
 					# We've found this recoded variable in this row.
 					# I want to now find what this codes for - so need to find the coding table for this variable
 					# from the outcome-info file, then look at what the uncoded version of the phenotype is.
-					var <- strsplit(rownames(pheno_summary)[where_reassignment[i]], split="_")[[1]][1]
+					var <- strsplit(pheno_summary[,1][where_reassignment[i]], split="_")[[1]][1]
 					where_var <- which(outcome_info$FieldID == var)
 
 					i_subname <- as.character(outcome_info$Coding[where_var])
@@ -258,7 +202,7 @@ include_PHESANT_reassignment_names <- function(pheno_summary, outcome_info)
 								get_subtype(strsplit(trim(to_parse[where_subvar])," ")[[1]]),
 								"PHESANT recoding")
 							
-							pheno_summary[where_reassignment[i],1] <- gsub("PHESANT recoding", i_subname, pheno_summary[where_reassignment[i],1])
+							pheno_summary[where_reassignment[i],2] <- gsub("PHESANT recoding", i_subname, pheno_summary[where_reassignment[i],2])
 						}
 					}
 				
@@ -268,7 +212,7 @@ include_PHESANT_reassignment_names <- function(pheno_summary, outcome_info)
 						where_coding <- which(codings_tables[coding][[1]]$coding == reassignments_before_after_list[[i]][j,1])
 						i_subname <- codings_tables[coding][[1]]$meaning[where_coding]
 
-						pheno_summary[where_reassignment[i],1] <- paste(pheno_summary[where_reassignment[i],1], i_subname, sep="")
+						pheno_summary[where_reassignment[i],2] <- paste(pheno_summary[where_reassignment[i],2], i_subname, sep="")
 					}
 				}
 			}
